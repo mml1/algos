c405fe20d7ab622ef5142bfa8aa209aa722a85f4
mml1 <mmlliguicota@gmail.com>
2016-07-12 17:04:15 -0700

    dyanmic programming version of fib

--- fibRd.js
+++ fibRd.js
@@ -1,6 +1,6 @@
 /*
-	Implement a recursive version of fib(n) (which returns the nth element of the Fibonacci sequence). 
-	Then implement a dynamic programming version of fib(n). 
+	Implement a recursive version of fib(n) (which returns the nth element of 
+	the Fibonacci sequence). Then implement a dynamic programming version of fib(n). 
 	What is the big-O runtime of each version? Why?
 */
 
@@ -16,3 +16,26 @@ function rFib(num){
 }
 
 // Part 2: Dynamic Programming of Fib
+
+var calculated = {};
+
+function fib(n){
+	if(n<=0){
+		return 0;
+	}
+	if(n<=2){
+		return 1;
+	}
+	// look for the number if it was calculated previously
+	for(num in calculated){
+		if (n == num){
+			return calculated[num];
+		}
+	}
+	// store the number
+	calculated[n] = fib(n-1) + fib(n-2);
+	return calculated[n];
+}
+//Testing
+console.log(fib(4));
+console.log(calculated)

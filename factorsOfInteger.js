98cc333d03e6956331c0e82be028fbdee3b69d15
mml1 <mmlliguicota@gmail.com>
2016-07-31 12:35:05 -0700

    did bonus pt

--- factorsOfInteger.js
+++ factorsOfInteger.js
@@ -30,4 +30,38 @@ console.log(factors(num));
 	Operations depend on the integer given.
 */
 
-// Need to try different nums to see how fast my code is running
+// Bonus: think of a way to improve performance
+
+
+var previousCalculated = {}
+function factors(integer){
+	// profiling function
+	console.time("Found factors in")
+
+	var listOfFactors = [];
+	//see if it was calculated before
+	if (integer in previousCalculated){
+		console.timeEnd("Found factors in")
+		return previousCalculated[integer];
+	}
+
+	// go through every number before the integer to see if it is a factor
+	for(var i = 0; i < integer; i++){
+		if(integer%i === 0){
+			listOfFactors.push(i);
+		}
+	}
+	previousCalculated[integer] = listOfFactors;
+	
+	// profiling function
+	console.timeEnd("Found factors in")
+	return listOfFactors;
+}
+
+//Testing
+var num = 100000;
+console.log(factors(num));
+
+
+var num = 100000;
+console.log(factors(num));
